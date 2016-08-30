7e1b00a84a5ddc6230aae102ad5818c31f4da676
spazer <lasercannon1@gmail.com>
2016-08-30 17:14:41 -0600

    Created form with ability to retrieve data from the smash.gg api. Only phase_group is supported atm.

---
+++ smashgg api/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace smashgg_api
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.buttonBracket = new System.Windows.Forms.Button();
+            this.buttonPhase = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 15);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(547, 170);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 191);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(267, 170);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 367);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(547, 170);
+            this.richTextBox3.TabIndex = 0;
+            this.richTextBox3.Text = "";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(582, 17);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(187, 20);
+            this.textBoxURL.TabIndex = 1;
+            // 
+            // buttonBracket
+            // 
+            this.buttonBracket.Location = new System.Drawing.Point(582, 43);
+            this.buttonBracket.Name = "buttonBracket";
+            this.buttonBracket.Size = new System.Drawing.Size(75, 23);
+            this.buttonBracket.TabIndex = 2;
+            this.buttonBracket.Text = "Get Bracket";
+            this.buttonBracket.UseVisualStyleBackColor = true;
+            this.buttonBracket.Click += new System.EventHandler(this.buttonBracket_Click);
+            // 
+            // buttonPhase
+            // 
+            this.buttonPhase.Location = new System.Drawing.Point(694, 43);
+            this.buttonPhase.Name = "buttonPhase";
+            this.buttonPhase.Size = new System.Drawing.Size(75, 23);
+            this.buttonPhase.TabIndex = 2;
+            this.buttonPhase.Text = "Get Phase";
+            this.buttonPhase.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(295, 191);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(267, 170);
+            this.richTextBox4.TabIndex = 0;
+            this.richTextBox4.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(603, 389);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(647, 389);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(603, 437);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 556);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.buttonPhase);
+            this.Controls.Add(this.buttonBracket);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button buttonBracket;
+        private System.Windows.Forms.Button buttonPhase;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ smashgg api/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace smashgg_api
+{
+    public partial class Form1 : Form
+    {
+        static string GG_URL_PHASEGROUP = "https://api.smash.gg/phase_group/";
+        static string GG_URL_PHASE = "https://api.smash.gg/phase/";
+        static string GG_URL_BRACKET = "?expand[]=sets&expand[]=entrants";
+        static string GG_URL_GROUPS = "?expand[]=groups";
+
+        static string GG_ENTRANTS = "\"entrants\":";
+        static string GG_SETS = "\"sets\":";
+
+        static string GG_ID = "\"id\":";
+        static string GG_GAMERTAG = "\"gamerTag\":";
+        static string GG_COUNTRY = "\"country\":";
+
+        Dictionary<string, Player> entrants = new Dictionary<string, Player>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBracket_Click(object sender, EventArgs e)
+        {
+            string webText;
+            int endPos = 0;
+
+            WebRequest r = WebRequest.Create(GG_URL_PHASEGROUP + textBoxURL.Text + GG_URL_BRACKET);
+            WebResponse resp = r.GetResponse();
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+            {
+                webText = sr.ReadToEnd();
+            }
+
+            string entrants = ExpandOnly(webText, GG_ENTRANTS, 0, out endPos);
+            GetEntrants(entrants);
+
+            string sets = ExpandOnly(webText, GG_SETS, 0, out endPos);
+        }
+
+        private string ExpandOnly(string input, string title, int startPos, out int endPos)
+        {
+            endPos = 0;
+            int bracketLevel = 0;
+            char openBracket;
+            char closeBracket;
+
+            // Find the beginning of the desired expand
+            if (input.IndexOf(title + "[", startPos) != -1)
+            {
+                openBracket = '[';
+                closeBracket = ']';
+
+                startPos = input.IndexOf(title + "[", startPos) + title.Length + 1;
+            }
+            else if (input.IndexOf(title + "{", startPos) != -1)
+            {
+                openBracket = '{';
+                closeBracket = '}';
+
+                startPos = input.IndexOf(title + "{", startPos) + title.Length + 1;
+            }
+            else
+            {
+                endPos = input.Length;
+                return string.Empty;
+            }
+
+            endPos = startPos;
+            bracketLevel = 1;
+
+            // Iterate through the text until the whole expand is acquired
+            do
+            {
+                int nextOpen = input.IndexOf(openBracket, endPos);
+                int nextClose = input.IndexOf(closeBracket, endPos);
+
+                // Bracket level cannot be zero if there are no more close brackets
+                if (nextClose == -1)
+                {
+                    endPos = input.Length;
+                    return string.Empty;
+                }
+
+                // Increase the bracket level for each additional open bracket. Subtract a level per close bracket.
+                if (nextOpen < nextClose && nextOpen != -1)
+                {
+                    bracketLevel++;
+                    endPos = nextOpen + 1;
+                }
+                else if (nextClose < nextOpen)
+                {
+                    bracketLevel--;
+                    endPos = nextClose + 1;
+                }
+            } while (bracketLevel > 0);
+
+            // Get whatever's in the brackets
+            string output = input.Substring(startPos, endPos - startPos - 1);
+
+            // If all we get are empty brackets, go deeper via recursion. If the end of input is reached, string.empty will be returned.
+            if (output == string.Empty)
+            {
+                output = ExpandOnly(input, title, endPos, out endPos);
+            }
+
+            return output;
+        }
+
+        private string SplitExpand(string input, int startPos, out int endPos)
+        {
+            int bracketLevel = 0;
+            endPos = startPos;
+
+            do
+            {
+                int nextOpen = input.IndexOf('{', endPos);
+                int nextClose = input.IndexOf('}', endPos);
+
+                // Bracket level cannot be zero if there are no more brackets
+                if (nextOpen == -1 || nextClose == -1)
+                {
+                    endPos = input.Length;
+                    return string.Empty;
+                }
+
+                // Increase the bracket level for each additional open bracket. Subtract a level per close bracket.
+                if (nextOpen < nextClose)
+                {
+                    bracketLevel++;
+                    endPos = nextOpen + 1;
+                }
+                else if (nextClose < nextOpen)
+                {
+                    bracketLevel--;
+                    endPos = nextClose + 1;
+                }
+            } while (bracketLevel > 0);
+
+            // Return whatever's in the brackets
+            return input.Substring(startPos, endPos - startPos - 1);
+        }
+
+        private int GetIntParameter(string input, string param)
+        {
+            if (input.IndexOf(param) != -1)
+            {
+                int start = input.IndexOf(param) + param.Length;
+                int length = 0;     // Current length of int
+                int temp;
+                int output = -99;
+
+                do 
+                {
+                    if (int.TryParse(input.Substring(start, length + 1), out temp))
+                    {
+                        output = temp;
+                        length++;
+                    }
+                    else
+                    {
+                        if (length == 0)
+                        {
+                            return -99;
+                        }
+                        else
+                        {
+                            return output;
+                        }
+                    }
+                } while (length < 15);  // Arbitrary limit
+
+                return -99;
+            }
+            else
+            {
+                return -99;
+            }
+        }
+
+        private string GetStringParameter(string input, string param)
+        {
+            if (input.IndexOf(param) != -1)
+            {
+                int start = input.IndexOf(param) + param.Length;
+
+                // Error check for things that are not strings
+                if(input.Substring(start,1) != "\"")
+                {
+                    return string.Empty;
+                }
+
+                // Find the closing quotation mark
+                start = start + 1;
+                int end = input.IndexOf("\"", start);
+
+                return input.Substring(start, end - start);
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        private void GetEntrants(string input)
+        {
+            List<string> rawEntrantData = new List<string>();
+            int endPos = 0;
+            string temp;
+
+            // Divide input into manageable chunks
+            do
+            {
+                temp = SplitExpand(input, endPos, out endPos);
+                if (temp != string.Empty)
+                {
+                    rawEntrantData.Add(temp);
+                }
+            } while (temp != string.Empty);
+
+            foreach (string entrant in rawEntrantData)
+            {
+                Player newPlayer = new Player();
+                // Get player ID
+                int id = GetIntParameter(entrant, GG_ID);
+
+                // Get player tag
+                newPlayer.name = GetStringParameter(entrant, GG_GAMERTAG);
+
+                // Get player country
+                newPlayer.country = GetStringParameter(entrant, GG_COUNTRY);
+            }
+        }
+
+        class Player
+        {
+            public int id;
+            public string name;
+            public string country;
+        }
+
+        class Team
+        {
+
+        }
+
+        class Set
+        {
+
+        }
+    }
+}
